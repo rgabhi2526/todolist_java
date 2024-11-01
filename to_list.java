0d6a06b5d9db6cef70d2be8a8fb93012abcc30e1
rgabhi2526 <143308195+rgabhi2526@users.noreply.github.com>
2024-11-01 12:29:22 +0530

    Update to_list.java

--- to_list.java
+++ to_list.java
@@ -1,9 +1,9 @@
 package project;
-
+// declearing the interface and the Methods the ToDoList contains 
 interface to_list {
-    void add_task(String a,String b);
+    void add_task(String a,String b);// a refers to the task while b refers to the time string 
     void remove_task(int i);
     void modify_task(int i,String a);
-    void print();
+    void print();// prints the task into a txt file 
 }
 
