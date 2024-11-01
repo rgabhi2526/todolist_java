471def43daedc151a4ac9448905e1c5cf7072d47
rgabhi2526 <143308195+rgabhi2526@users.noreply.github.com>
2024-11-01 12:22:49 +0530

    Update src.java

--- src.java
+++ src.java
@@ -3,25 +3,28 @@ import java.util.*;
 import java.io.*;
 import java.text.*;
 
-
+// this is the source file of the to do list program implementing the to_list interface
 public class src implements to_list 
 {
-    List<String> arr;
-    ArrayList<String> arr_time;
+    List<String> arr;// arraylist that would be containing the tasks 
+    ArrayList<String> arr_time;//arraylist containing the time associated to the task 
     public src()
-    {
+    {// constructor of the class 
         arr=new ArrayList<>();
         arr_time=new ArrayList<>();
     }
     public void add_task(String a,String b)
     {
+        // a => Task % b => Time 
         String msg=a;
         try
         {
+            //checks whether time string is in proper format , if not an excpetion is thrown 
         SimpleDateFormat inputFormat = new SimpleDateFormat("HH:mm");
         SimpleDateFormat outputFormat = new SimpleDateFormat("hh:mm a");
         Date date = inputFormat.parse(b);
         String formattedTime = outputFormat.format(date);
+        // if the String b was in proper format then only the tasks would be added to arraylist 
         arr.add(msg);
         arr_time.add(formattedTime);
         }
@@ -32,43 +35,49 @@ public class src implements to_list
     }
     public void remove_task(int i)
     {
+        // this methods accepts an int argument which is the index of the task 
         try {
             String temp= arr.remove(i);
             String temp_time =arr_time.remove(i);
             SimpleDateFormat inputFormat = new SimpleDateFormat("HH:mm");
             SimpleDateFormat outputFormat = new SimpleDateFormat("hh:mm a");
             Date date = inputFormat.parse(temp_time);
-            String formattedTime = outputFormat.format(date);
+            String formattedTime = outputFormat.format(date);// converts time into 12 hr format 
             System.out.println("Task Removed: "+temp+" at "+formattedTime);
-            System.out.println("Task successfully removed !!");
         } catch (Exception e) {
+            // if the index is out of bound an exception is thrown 
             System.err.println("Error :"+e );
         }
     }
-    public void modify_task(int i,String a)
+    public void modify_task(int i,String a) // this method takes an int argument for index and another String argument to change the task 
     {
         try {
             arr.remove(i);
             arr.add(i,a);
         } catch (Exception e) {
+            // if the index is out of bound an exception is thrown 
             System.err.println(e);
         }
     }
-    public void modify_task(int i,String a,String b)
+    public void modify_task(int i,String a,String b)// method overloading -> this method takes an int argument for index and another String argument to change the task 
+                                                    // while the 2nd String argument is for changing the time 
     {
           try {
             SimpleDateFormat inputFormat = new SimpleDateFormat("HH:mm");
             SimpleDateFormat outputFormat = new SimpleDateFormat("hh:mm a");
             Date date = inputFormat.parse(b);
             String formattedTime = outputFormat.format(date);
+            //first the already assigned task at the task is removed and then the new task is placed in place of the deleted task
             arr.remove(i);
             arr_time.remove(i);
             arr.add(i,a);
             arr_time.add(i, formattedTime);
           } catch (Exception e) {
+            // if the time format is wrong or the index is outofbound 
             System.err.println(e);
           }
     }
+
     public void print()
     {
         File currentDirectory = new File("to_list.txt");
@@ -77,10 +86,11 @@ public class src implements to_list
         {
             //file already doesn't exists 
             FileWriter writer =new FileWriter("to_list.txt");
-            writer.write("Task\t\tTime");
             for(int i=0;i<arr.size();i++)
             {
-                writer.write(arr.get(i)+"\t\t"+arr_time.get(i));
+                // prints in the file in the following format :
+                // Task {i} : {Task}    Time : {Time}
+                writer.write("Task "+(i+1)+":"+arr.get(i)+"\t\t"+"Time "+":"+arr_time.get(i));
             }
             writer.close();
 
@@ -88,10 +98,11 @@ public class src implements to_list
         else{
             //file already exists 
             FileWriter writer =new FileWriter("to_list.txt");
-            writer.write("Task\t\tTime");
             for(int i=0;i<arr.size();i++)
             {
-                writer.write(arr.get(i)+"\t\t"+arr_time.get(i)+"\n");
+                // prints in the file in the following format :
+                // Task {i} : {Task}    Time : {Time}
+                writer.write("Task "+(i+1)+":"+arr.get(i)+"\t\t"+"Time "+":"+arr_time.get(i)+"\n");
 
             }
             writer.close();
@@ -100,15 +111,17 @@ public class src implements to_list
     }
     catch(Exception e)
     {
+        // if an IOexception or Security Exception is thrown 
         e.printStackTrace();
     }
 }
     public void display()
     {
-        System.out.print("Task\t\tTime");
         for(int i=0;i<arr.size();i++)
             {
-                System.out.print(arr.get(i)+"\t\t"+arr_time.get(i)+"\n");
+                // prints in the file in the following format :
+                // Task {i} : {Task}    Time : {Time}
+                System.out.println("Task "+(i+1)+":"+arr.get(i)+"\t\t"+"Time "+":"+arr_time.get(i));
             }
     }
 }
